8f8c1d1df8c9439478b3d3e1eafde98f9b7ca3e5
satchelsmidt <34499356+satchelsmidt@users.noreply.github.com>
2020-01-20 22:58:08 -0800

    removed unneccessary assets, made box styling consistent across pages

--- satchel-portfolio/src/components/about.js
+++ satchel-portfolio/src/components/about.js
@@ -4,7 +4,7 @@ import Rainbow from '../hoc/Rainbow'
 const About = () => {
     return (
         <div className="row">
-           <div className="aboutText card amber lighten-5 col l8 m7 s12 offset-m5 offset-l4">
+                <div className="aboutText card amber lighten-5 col l8 m9 s12 offset-m3 offset-l4">
                 <h5>About Me</h5>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 <br></br>
