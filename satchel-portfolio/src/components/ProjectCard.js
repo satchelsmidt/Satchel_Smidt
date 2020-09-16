7c07ef0b15c01d539fdd1d195ba69785d1aa2a56
satchelsmidt <satchelsmidt@gmail.com>
2020-09-16 11:45:45 -0700

    fixed card layout

--- satchel-portfolio/src/components/ProjectCard.js
+++ satchel-portfolio/src/components/ProjectCard.js
@@ -6,20 +6,18 @@ class ProjectCard extends Component {
     render() {
 
         return (
-            <div className="col l6 m12 s12 cardCol">
-                <div className="card projectCard amber lighten-5">
-                    <a href={this.props.deployed} className="deployedLink" target="_blank" rel="noopener noreferrer"><h3 className="card-title">{this.props.name}</h3></a>
-                    <div className="card-image">
-                        <a href={this.props.deployed} className="deployedLink" target="_blank" rel="noopener noreferrer"><img className="projectPic" src={require('../assets/' + this.props.img)} alt="project"/></a>
-                        <a className="btn-floating halfway-fab waves-effect waves-light white" href={this.props.github} target="_blank" rel="noopener noreferrer"><img src={Github} className="githubSmall" alt="github"></img></a>
+            <div className="card projectCard amber lighten-5" style={styles.cardStyle}>
+                <a href={this.props.deployed} className="deployedLink" target="_blank" rel="noopener noreferrer"><h3 className="card-title">{this.props.name}</h3></a>
+                <div className="card-image">
+                    <a href={this.props.deployed} className="deployedLink" target="_blank" rel="noopener noreferrer"><img className="projectPic" src={require('../assets/' + this.props.img)} alt="project" /></a>
+                    <a className="btn-floating halfway-fab waves-effect waves-light white" href={this.props.github} target="_blank" rel="noopener noreferrer"><img src={Github} className="githubSmall" alt="github"></img></a>
+                </div>
+                <div className="card-stacked">
+                    <div className="card-content">
+                        <p>{this.props.description}</p>
                     </div>
-                    <div className="card-stacked">
-                        <div className="card-content">
-                            <p>{this.props.description}</p>
-                        </div>
-                        <div className="card-action">
-                            <p className="projectDesc">Built with: {this.props.technologies.join(", ")}</p>
-                        </div>
+                    <div className="card-action">
+                        <p className="projectDesc">Built with: {this.props.technologies.join(", ")}</p>
                     </div>
                 </div>
             </div>
@@ -28,4 +26,12 @@ class ProjectCard extends Component {
 }
 
 export default ProjectCard
+
+const styles = {
+    cardStyle: {
+        width: '40%',
+        margin: '10px',
+        padding: '15px'
+    }
+}
 
--- satchel-portfolio/src/components/pages/ProjectPage.js
+++ satchel-portfolio/src/components/pages/ProjectPage.js
@@ -1,13 +1,35 @@
 import React from 'react'
-import Projects from '../ProjectRow'
+import ProjectCard from '../ProjectCard'
+import projects from '../../projects.json'
 
 const ProjectPage = () => {
+
+    const renderProjects = projects.map((project, index) => {
+        return (
+            <ProjectCard key={index} name={project.name} description={project.description} technologies={project.technologies} github={project.github_link} deployed={project.deployed_link} img={project.img_source} />
+        )
+    })
+
     return (
         <div className="row">
             <h5 className="projectHeader">Projects</h5>
-            <Projects />
+            <div style={styles.projectContainer}>
+                {renderProjects}
+            </div>
         </div>
     )
 }
 
 export default ProjectPage
+
+const styles = {
+    projectContainer: {
+        'marginTop': '10px',
+        'display': 'flex',
+        'flexWrap': 'wrap',
+        'flexDirection': 'row',
+        'justifyContent': 'space-around',
+        'alignItems': 'center',
+        'height': 'auto',
+    }
+}
